7e0745acec48dcfd197e5451513f326b45f69968
Genaro Tapia <genaro@sicar.mx>
2024-10-23 13:14:12 -0600

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "sxt-jenkins"
+
---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+
+    @Test
+    @DisplayName("Simple Test")
+    public void simpleTest() {
+        System.out.println("SUCCESS");
+    }
+}
