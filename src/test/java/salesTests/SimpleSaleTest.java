15614bf7a67891f590acef1e0cd7e4e24effa1a3
Genaro Tapia <genaro@sicar.mx>
2024-10-24 16:28:45 -0600

    simple sale test

--- src/test/java/salesTests/SimpleSaleTest.java
+++ src/test/java/salesTests/SimpleSaleTest.java
@@ -4,10 +4,16 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class SimpleSaleTest {
-
+//    salesTests.SimpleSaleTest,    purchasesTests.*
     @Test
     @DisplayName("Simple Sale Test")
     public void simpleTest() {
+        System.out.println("SUCCESS");
+    }
+
+    @Test
+    @DisplayName("Simple Sale Test 2")
+    public void simpleTest2() {
         System.out.println("SUCCESS");
     }
 }
