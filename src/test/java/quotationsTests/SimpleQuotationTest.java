be56093cb602647f9667cc3de3916c009c4cc417
Genaro Tapia <genaro@sicar.mx>
2024-10-24 10:52:46 -0600

    fail test

---
+++ src/test/java/quotationsTests/SimpleQuotationTest.java
@@ -0,0 +1,14 @@
+package quotationsTests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SimpleQuotationTest {
+
+    @Test
+    @DisplayName("Simple Quotation Test")
+    public void simpleTest() {
+        Assertions.fail();
+    }
+}
